f2cdd6ba47df5ccd49bb966134cbd99fca76fd03
María Gárgoles <mariagargoles@live.com>
2024-04-26 12:39:27 +0200

    separate

--- js/coinconv.js
+++ js/coinconv.js
@@ -1,12 +1,21 @@
-const CoinConversion = () => {};
-
-const PeticionApi = () => {
+const CoinConversion = () => {
   const buttons = document.querySelectorAll("#coins button");
-
   buttons.forEach((button) => {
     button.addEventListener("click", () => {
       console.log("Se hizo clic en el botón de la moneda", button.id);
     });
+  });
+};
+
+const PeticionApi = () => {
+  const url =
+    "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json";
+
+  fetch(url).then((response) => {
+    if (response.ok) {
+      response.json();
+      console.log(response.json);
+    }
   });
 };
 
