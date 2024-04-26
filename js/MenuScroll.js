b0ef86e04cc0fd395d7c438f2219c829415be5ae
María Gárgoles <mariagargoles@live.com>
2024-04-26 11:29:11 +0200

    Finish modal validation and send data to API

--- js/MenuScroll.js
+++ js/MenuScroll.js
@@ -1,6 +1,12 @@
-const scrollbar = document.getElementById("scroll__progress");
+window.addEventListener("load", () => {
+  const scrollbar = document.getElementById("scroll__progress");
+  requestAnimationFrame(scroll);
+});
 
-function update() {
-    scrollbar.style.width = `${(window.scrollY) / (document.body.scrollHeight - window.innerHeight) * 100}%`;
-    
+function scroll() {
+  const scrollbar = document.getElementById("scroll__progress");
+  scrollbar.style.width = `${
+    (window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100
+  }%`;
+  requestAnimationFrame(scroll);
 }
