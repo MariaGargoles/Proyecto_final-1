8eb0d8d06222690a8ceeaae0fbf9882a4f58c54c
María Gárgoles <mariagargoles@live.com>
2024-04-29 22:09:40 +0200

    fix and style slide movile

--- js/HambMenu.js
+++ js/HambMenu.js
@@ -1,11 +1,12 @@
 const DisplayMenu = () => {
-    const HamburguerButton = document.getElementById("FirstBottomMenu");    
-    const HamburguerMenu = document.getElementById("NavarHambuguerMenu");
+  const HamburguerButton = document.getElementById("FirstBottomMenu");
+  const HamburguerMenu = document.getElementById("NavarHambuguerMenu");
+  const CloseHamburguerMenu = document.getElementById("CloseBottomMenu");
 
-    HamburguerButton.addEventListener("click", (event) => {
-        console.log("Click done")
-        HamburguerMenu.classList.toggle("ContainerNav__Section__2--active");
-    });
-}
+  HamburguerButton.addEventListener("click", (event) => {
+    console.log("Click done");
+    HamburguerMenu.classList.toggle("ContainerNav__Section__2--active");
+  });
+};
 
 DisplayMenu();
