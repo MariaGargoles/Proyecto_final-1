2618aeb446518099585cc4de1d053711dad44a6c
María Gárgoles <mariagargoles@live.com>
2024-05-01 09:08:18 +0200

    add close button to nav menu

--- js/HambMenu.js
+++ js/HambMenu.js
@@ -1,11 +1,11 @@
 const DisplayMenu = () => {
   const HamburguerButton = document.getElementById("FirstBottomMenu");
   const HamburguerMenu = document.getElementById("NavarHambuguerMenu");
-  const CloseHamburguerMenu = document.getElementById("CloseBottomMenu");
 
   HamburguerButton.addEventListener("click", (event) => {
     console.log("Click done");
     HamburguerMenu.classList.toggle("ContainerNav__Section__2--active");
+    HamburguerButton.classList.toggle("ContainerNav__Section__1__img--off");
   });
 };
 
